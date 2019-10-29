bb6886c359aafbc15e8cd7d2395368c70aee942f
Robert Pierce <pierce1673@gmail.com>
2019-10-29 18:53:39 -0400

    commenting out start over button for now since i can't get the game to reset correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ let questions = [
   {
     question:
       "What does AD stand for in relation to Windows Operating Systems?",
-    answers: [
+    answerArray: [
       "Alternative Drive",
       "Active Directory",
       "Automated Database",
@@ -13,42 +13,52 @@ let questions = [
   },
   {
     question: "What five letter word is the motto of the IBM Computer company?",
-    answers: ["Click", "Logic", "Think", "Sweet"],
+    answerArray: ["Click", "Logic", "Think", "Sweet"],
     correctAnswer: ["Think"]
   },
   {
     question:
       "Which of the following languages is used as a scripting language in the Unity 3D game engine?",
-    answers: ["Java", "C++", "C#", "Vulcan"],
+    answerArray: ["Java", "C++", "C#", "Vulcan"],
     correctAnswer: ["C#"]
   },
   {
     question:
       "Nvidia&#039;s headquarters are based in which Silicon Valley city?",
-    answers: ["Palo Alto", "Cupertino", "Santa Clara", "The City by the Bay"],
+    answerArray: [
+      "Palo Alto",
+      "Cupertino",
+      "Santa Clara",
+      "The City by the Bay"
+    ],
     correctAnswer: ["Santa Clara"]
   },
   {
     question:
       "Which one of these is not an official development name for a Ubuntu release?",
-    answers: ["Modest Mouse", "Trusty Tahr", "Utopic Unicorn", "Wily Werewolf"],
+    answerArray: [
+      "Modest Mouse",
+      "Trusty Tahr",
+      "Utopic Unicorn",
+      "Wily Werewolf"
+    ],
     correctAnswer: ["Modest Mouse"]
   },
   {
     question:
       "What is the name of the default theme that is installed with Windows XP?",
-    answers: ["Neptune", "Bliss", "Luna", "Bonkerzzz!"],
+    answerArray: ["Neptune", "Bliss", "Luna", "Bonkerzzz!"],
     correctAnswer: ["Luna"]
   },
   {
     question:
       "Which of the following is a personal computer made by the Japanese company Fujitsu?",
-    answers: ["FM-7", "PC-9801", "Xmillennium", "Apple IIc"],
+    answerArray: ["FM-7", "PC-9801", "Xmillennium", "Apple IIc"],
     correctAnswer: ["FM-7"]
   },
   {
     question: "What does the term MIME stand for, in regards to computing?",
-    answers: [
+    answerArray: [
       "Multipurpose Internet Mail Extensions",
       "Mail Internet Mail Exchange",
       "Mail Interleave Method Exchange",
@@ -58,7 +68,7 @@ let questions = [
   },
   {
     question: "What data storage technology is abbreviated RAID?",
-    answers: [
+    answerArray: [
       "Rapid Access for Indexed Devices",
       "Redundant Array of Independent Disks",
       "Romeo Alpha Indigo Delta",
@@ -68,7 +78,7 @@ let questions = [
   },
   {
     question: "How many bytes are in a single Kibibyte?<br/>",
-    answers: ["1000", "1024", "1240", "So, So Many"],
+    answerArray: ["1000", "1024", "1240", "So, So Many"],
     correctAnswer: ["1024"]
   }
 ];
@@ -107,14 +117,14 @@ let game = {
       $("#box").append(
         "<h2 class='question'>" + questions[i].question + "</h2>"
       );
-      for (let j = 0; j < questions[i].answers.length; j++) {
+      for (let j = 0; j < questions[i].answerArray.length; j++) {
         $("#box").append(
           "<input type='radio' name='question-" +
             i +
             "' value='" +
-            questions[i].answers[j] +
+            questions[i].answerArray[j] +
             "'>" +
-            questions[i].answers[j] +
+            questions[i].answerArray[j] +
             "<br/>"
         );
       }
@@ -213,10 +223,10 @@ let game = {
         "</h3>"
     );
     // Start over button
-    $("#box").append('<button id="startOver" class="done">Play Again</button>');
-    $("#startOver").on("click", function() {
-      $(".done").remove();
-      game.start();
-    });
+    // $("#box").append('<button id="startOver" class="done">Play Again</button>');
+    // $("#startOver").on("click", function() {
+    //   $(".done").remove();
+    //   game.start();
+    // });
   }
 };

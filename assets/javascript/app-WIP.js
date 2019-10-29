d6458de4b6d4fde902514ef9dc70a098cab39bab
Robert Pierce <pierce1673@gmail.com>
2019-10-29 01:05:44 -0400

    javascript updates + styling

--- assets/javascript/app-WIP.js
+++ assets/javascript/app-WIP.js
@@ -1,4 +1,3 @@
-// Silly questions
 let questions = [
   {
     question:
@@ -37,7 +36,7 @@ let questions = [
   {
     question:
       "What is the name of the default theme that is installed with Windows XP?",
-    answers: ["Neptune", "Bliss", "Luna", "Bonkerzzz!"],
+    answers: ["Neptune", "Bliss", "Luna", "Bonkers!"],
     correctAnswer: ["Luna"]
   },
   {
@@ -57,7 +56,7 @@ let questions = [
     correctAnswer: ["Multipurpose Internet Mail Extensions"]
   },
   {
-    question: "What data storage technology is abbreviated RAID?",
+    question: "What does RAID stand for?",
     answers: [
       "Rapid Access for Indexed Devices",
       "Redundant Array of Independent Disks",
@@ -67,46 +66,70 @@ let questions = [
     correctAnswer: ["Redundant Array of Independent Disks"]
   },
   {
-    question: "How many bytes are in a single Kibibyte?<br/>",
+    question: "How many bytes are in a single Kibibyte?",
     answers: ["1000", "1024", "1240", "So, So Many"],
     correctAnswer: ["1024"]
   }
 ];
 
-// Click events
-$("#start").on("click", function() {
+$("#start").on("click", () => {
   game.start();
 });
 
-$(document).on("click", "#end", function() {
+$(document).on("click", "#end", () => {
   //   console.log("done?");
   game.done();
 });
 
-// Results + Countdown timer
 let game = {
   correct: 0,
+  currentQuestion: 0;
   incorrect: 0,
-  counter: 120,
-  countdown: function() {
-    game.counter--;
-    $("#counter").html(game.counter);
-    if (game.counter === 0) {
-      // console.log("done?");
-      game.done();
+  counter: 10,
+  countdown: () => {
+    game.counter--
+    $("#counter").html(game.conter);
+    if(game.counter <= 0){
+        console.log("Time is up")
+        game.timeUp();
     }
   },
-  // Start of game
-  start: function() {
+  loadQuestion: () => {
+    timer = setInterval(game.countdown, 1000);
+    $("#box").html("<h2>" + question[game.currentQuestion].question + "</h2>");
+    for(let i = 0; i < questions[game.currentQuestion].answers.length; i++);
+    $("#box").append("<button>")
+},
+nextQuestion: () => {
+  
+},
+timeUp: () => {
+  
+},
+results: () => {
+  
+},
+clicked: () => {
+  
+},
+answeredCorrectly: () => {
+  
+},
+answeredIncorrectly: () => {
+  
+},
+reset: () => {
+  
+}
+
+  start: () => {
     timer = setInterval(game.countdown, 1000);
     $("#box").prepend(
       "<h2>Time Remaining: <span id='counter'>120</span> Seconds</h2>"
     );
     $("#start").remove();
     for (let i = 0; i < questions.length; i++) {
-      $("#box").append(
-        "<h2 class='question'>" + questions[i].question + "</h2>"
-      );
+      $("#box").append("<h2>" + questions[i].question + "</h2>");
       for (let j = 0; j < questions[i].answers.length; j++) {
         $("#box").append(
           "<input type='radio' name='question-" +
@@ -114,109 +137,96 @@ let game = {
             "' value='" +
             questions[i].answers[j] +
             "'>" +
-            questions[i].answers[j] +
-            "<br/>"
+            questions[i].answers[j]
         );
       }
     }
-    $("#box").append('<br/><button id="end">DONE</button>');
+    $("#box").append('<button id="end">DONE</button>');
   },
-  // Done function
-  done: function() {
-    $.each($("input[name='question-0']:checked"), function() {
+  done: () => {
+    $.each($("input[name='question-0']:checked"), () => {
       if ($(this).val() == questions[0].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-1']:checked"), function() {
+    $.each($("input[name='question-1']:checked"), () => {
       if ($(this).val() == questions[1].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-2']:checked"), function() {
+    $.each($("input[name='question-2']:checked"), () => {
       if ($(this).val() == questions[2].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-3']:checked"), function() {
+    $.each($("input[name='question-3']:checked"), () => {
       if ($(this).val() == questions[3].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-4']:checked"), function() {
+    $.each($("input[name='question-4']:checked"), () => {
       if ($(this).val() == questions[4].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-5']:checked"), function() {
+    $.each($("input[name='question-5']:checked"), () => {
       if ($(this).val() == questions[5].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-6']:checked"), function() {
+    $.each($("input[name='question-6']:checked"), () => {
       if ($(this).val() == questions[6].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-7']:checked"), function() {
+    $.each($("input[name='question-7']:checked"), () => {
       if ($(this).val() == questions[7].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-8']:checked"), function() {
+    $.each($("input[name='question-8']:checked"), () => {
       if ($(this).val() == questions[8].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($("input[name='question-9']:checked"), function() {
+    $.each($("input[name='question-9']:checked"), () => {
       if ($(this).val() == questions[9].correctAnswer) {
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    // Compilation of results
-    this.result();
   },
-  // Display of results
-  result: function() {
+
+  result: () => {
     clearInterval(timer);
     $("#box h2").remove();
-    $("#box").html("<h2 class='done'>All Done!</h2>");
+    $("#box").html("<h2>All Done!</h2>");
+    $("#box").append("<h3>Correct Answers: " + this.correct + "</h3>");
+    $("#box").append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
     $("#box").append(
-      "<h3 class='done'>Correct Answers: " + this.correct + "</h3>"
-    );
-    $("#box").append(
-      "<h3 class='done'>Incorrect Answers: " + this.incorrect + "</h3>"
-    );
-    $("#box").append(
-      "<h3  class='done'>Unanswered: " +
-        (questions.length - (this.incorrect + this.correct)) +
+      "<h3>Unanswered: " +
+        (questions.length + (this.incorrect + this.correct)) +
         "</h3>"
     );
-    // Start over button
-    $("#box").append('<button id="startOver" class="done">Start Over</button>');
-    $("#startOver").on("click", function() {
-      $(".done").remove();
-      game.start();
-    });
+    this.result();
   }
 };
